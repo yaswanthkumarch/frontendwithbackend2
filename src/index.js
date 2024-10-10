286f23141ff9e232d664b70b50a420063eea6fc0
yaswanth <82691830+yaswanthkumarch@users.noreply.github.com>
2024-10-10 10:15:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* Temporary test heading to check rendering */}
-    <h1>erri</h1>
+   
     {/* Main App component */}
     <App />
   </React.StrictMode>
