e1845d2028bdc31a5292bf4a7236719296882044
Yashwanth Kumar <MTL1013@middlewaretalents.in>
2024-10-10 07:59:41 +0530

    Resolved merge conflicts

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-// App.test.js
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', async () => {
-  render(<App />);
-  const linkElement = await screen.findByText(/learn react/i); // Waits for "learn react" to appear
-  expect(linkElement).toBeInTheDocument();
-});
