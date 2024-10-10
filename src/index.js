b661a8f1b58d08fb826f0b7d5f97a2b41baf403b
yaswanth <82691830+yaswanthkumarch@users.noreply.github.com>
2024-10-10 09:51:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <h1>erri</h1>
     <App />
   </React.StrictMode>
 );
